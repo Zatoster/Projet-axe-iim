f49eba198aa14e45ad8742da541e6070b9cf9eed
Zatoster <Zatoster.mc@gmail.com>
2022-04-19 16:07:24 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<header>
+    <div>
+        <img src="" alt=""> <!--logo-->
+        <div>
+            <ul>
+                <li>
+                    <a href="">bonjour</a>
+                </li>
+                <li>
+                    <a href="">bonjour2</a>
+                </li>
+                <li>
+                    <a href="">bonjour3</a>
+                </li>
+                <li>
+                    <a href="">bonjour4</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div>
+        <a href="">login</a>
+        <a href="">register</a>
+    </div>
+</header>
+<main>
+    <div>
+        <h1>Titre</h1>
+        <h2>Presentation</h2>
+        <div class="maintext">
+            <p>
+                Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. Sed massa massa,
+                sodales sed scelerisque non, facilisis sit amet dui.
+                Nulla laoreet eget felis nec condimentum. Nam vel nisl sit amet tortor aliquam dignissim ut sed tortor.
+                Maecenas tempus dolor efficitur, facilisis nunc porta, feugiat arcu.
+                Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                Proin ligula nisl, dignissim sed dui ac, suscipit porta lectus. Praesent gravida nisl a massa dignissim,
+                nec porttitor purus dapibus. Aliquam erat volutpat. Duis rutrum nulla a posuere hendrerit.
+            </p>
+            <img src="images/Placeholder250px.webp" alt="">
+        </div>
+    </div>
+</main>
+<footer>
+
+</footer>
+</body>
+</html>
